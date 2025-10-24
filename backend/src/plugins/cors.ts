90e47c0c1565f40466606e9a5c327f53406c7bbc
Rohit kumar yadav <rohitkuyada@gmail.com>
2025-10-24 18:35:24 +0530

    feat: update CORS allowed origins to include production URLs

--- backend/src/plugins/cors.ts
+++ backend/src/plugins/cors.ts
@@ -4,7 +4,7 @@ import cors from '@fastify/cors'
 export default async function corsPlugin(app: FastifyInstance) {
     await app.register(cors, {
         origin: (origin, callback) => {
-            const allowedOrigins = ['http://localhost:3000', 'http://localhost:3001'];
+            const allowedOrigins = ['http://localhost:3000', 'http://localhost:3001', 'https://cleardrip-main.vercel.app', 'https://cleardrip.in/'];
             if (!origin || allowedOrigins.includes(origin)) {
                 callback(null, true);
             } else {
